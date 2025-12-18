8abc53a4fe8bfd0038fa09e1eaccbc0d3a547526
Vang Pham Trong <55351794+vangpt@users.noreply.github.com>
2025-12-18 14:12:23 +0700

    Implement manageCreator function for TikTok page

---
+++ src/pages/ManageCreator.js
@@ -0,0 +1,32 @@
+export async function manageCreator(page) {
+  await page.goto(
+    "https://affiliate.tiktok.com/connection/creator-management?view_blocked_creator=&shop_region=VN",
+    {
+      waitUntil: "domcontentloaded",
+    }
+  );
+
+  await page
+    .locator("button", {
+      has: page.locator("span", {
+        hasText: "Mời hàng loạt",
+      }),
+    })
+    .click();
+
+  // await page.locator("div[title='20/Trang']").click();
+
+  // await page
+  //   .locator("span.arco-select-option-content-wrapper", {
+  //     hasText: "50/Trang",
+  //   })
+  //   .click();
+
+  await page.locator("thead div.arco-checkbox-mask").click();
+
+  await page
+    .getByRole("button", {
+      name: "Mời cộng tác",
+    })
+    .click();
+}
